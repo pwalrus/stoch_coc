0af46afcfcd82dd5d5cc3eacbdeab05d9cda0cc4
p walrus <possible_walrus@protonmail.com>
2022-09-02 19:24:30 +0000

    unpacking terms recursively to star

--- src/unpack_term.rs
+++ src/unpack_term.rs
@@ -1,6 +1,19 @@
 use crate::model::judgement::{Judgement, Statement};
 use crate::model::expression::{CCExpression};
 
+fn unpack_star(context: &[Statement]) -> Vec<Judgement> {
+    let stmt = Statement {
+        subject: CCExpression::Star,
+        s_type: CCExpression::Sq
+    };
+
+    return vec![Judgement {
+        statement: stmt,
+        context: context.to_vec()
+    }];
+}
+
+
 fn unpack_var(var: &str, context: &[Statement]) -> Vec<Judgement> {
     let v_type: Option<CCExpression> = context.iter().filter_map(
         |st| if st.subject.var_str() == Some(var.to_string()) {
@@ -8,15 +21,23 @@ fn unpack_var(var: &str, context: &[Statement]) -> Vec<Judgement> {
         } else {
             return None;
         }).next();
+
     if let Some(s_type) = v_type {
         let stmt = Statement {
             subject: CCExpression::Var(var.to_string()),
             s_type: s_type
         };
-        return vec![Judgement {
+        let ctx2: Vec<Statement> = context.iter().filter_map(
+            |st| if st.subject.var_str() == Some(var.to_string()) {
+                return None;
+            } else {
+                return Some(st.clone());
+            }).collect();
+
+        return [unpack_term(&stmt.s_type, &ctx2), vec![Judgement {
             statement: stmt,
             context: context.to_vec()
-        }];
+        }]].concat();
     }
     return vec![];
 }
@@ -24,7 +45,7 @@ fn unpack_var(var: &str, context: &[Statement]) -> Vec<Judgement> {
 pub fn unpack_term(term: &CCExpression, context: &[Statement]) -> Vec<Judgement> {
 
     match term {
-        CCExpression::Star => vec![],
+        CCExpression::Star => unpack_star(context),
         CCExpression::Sq => vec![],
         CCExpression::Var(x) => unpack_var(&x, context),
         CCExpression::Abs(_, _, _) => vec![],
@@ -41,17 +62,19 @@ mod tests {
 
     #[test]
     fn simple_unpack() {
-        let jdg: Judgement = parse_judgement("a:A \\vdash a:A").unwrap();
-        assert_eq!(jdg.to_latex(), "a : A \\vdash a : A");
+        let jdg: Judgement = parse_judgement("A:\\ast,a:A \\vdash a:A").unwrap();
+        assert_eq!(jdg.to_latex(), "A : \\ast, a : A \\vdash a : A");
         let lines = unpack_term(&jdg.statement.subject, &jdg.context);
 
-        assert_eq!(lines.len(), 1);
-        
+        assert_eq!(lines.len(), 3);
+
         let str_lines: Vec<String> = lines.iter().map(
             |x| x.to_latex()
             ).collect();
         assert_eq!(str_lines, [
-                   "a : A \\vdash a : A"
+                   "\\vdash \\ast : \\square",
+                   "A : \\ast \\vdash A : \\ast",
+                   "A : \\ast, a : A \\vdash a : A"
         ]);
     }
 }
