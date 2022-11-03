77019fbf7f3c8b05b3173a50ee946ab02351a45d
p walrus <possible_walrus@protonmail.com>
2022-11-03 14:48:17 -0600

    more reliable usued var finding in strategy

--- src/search/proof/strategy/untypeabs.rs
+++ src/search/proof/strategy/untypeabs.rs
@@ -10,22 +10,23 @@ pub struct UnpackTypeAbs {}
 
 impl ProofStrat for UnpackTypeAbs {
     fn sub_goals(&self, ex: &CCExpression,
-                 _: &[Statement],
+                 context: &[Statement],
                  inner_context: &[Statement],
                  _: &[Judgement],
                  _: &[Definition]) -> Result<Vec<Goal>, String> {
         if let CCExpression::TypeAbs(var, a_type, ret) = ex {
+            let new_var = Statement::next_unused_var(&[context, inner_context].concat());
             let new_stmt = Statement {
-                subject: CCExpression::Var(var.to_string()),
+                subject: CCExpression::Var(new_var.to_string()),
                 s_type: *a_type.clone()
             };
             let subs = vec![
                 Goal::Initial(
-                    *ret.clone(),
+                    ret.substitute(var, &new_stmt.subject),
                     [inner_context, &[new_stmt]].concat())
             ];
             Ok(vec![Goal::Unpacked(CCExpression::Abs(
-                                       var.to_string(),
+                                       new_var.to_string(),
                                        Box::new(*a_type.clone()),
                                        Box::new(CCExpression::Var("sub_{0}".to_string()))),
                                    CCExpression::TypeAbs(
@@ -58,7 +59,7 @@ mod tests {
             Ok(lst) => {
                 assert_eq!(lst.len(), 1);
                 if let Goal::Unpacked(inst, ex, subs, inner) = &lst[0] {
-                    assert_eq!(inst.to_latex(), "\\lambda x : A . sub_{0}");
+                    assert_eq!(inst.to_latex(), "\\lambda a : A . sub_{0}");
                     assert_eq!(ex.to_latex(), "A \\to A");
                     assert_eq!(inner, &[]);
                     if let Goal::Initial(ex, _) = &subs[0] {
